9ac77515d04f4c768a153c4fe3c8031673cb250f
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2017-11-29 14:38:02 -0800

    Added tests and functions to pass array of text files through test

---
+++ __test__/file-reader.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const fileReader = require('../lib/file-reader');
+
+describe('file-reader.test.js', () => {
+  test('All text found', (done) => {
+    fileReader.readFile(['kafka.txt', 'panagram.txt', 'werther.txt'], (error, data) => {
+      expect(error).toBeNull();
+      expect(data).toEqual(['One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.', 'The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog.', ' wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.']);
+      done();
+    });
+  });
+});
